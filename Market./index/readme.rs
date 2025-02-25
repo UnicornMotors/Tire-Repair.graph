3dac12b798cf6e7992dc48286afc6ea8485da309
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-25 03:56:40 -0600

    Create readme.rs

---
+++ Market./index/readme.rs
@@ -0,0 +1,3 @@
+let search = ['https://www.aliexpress.us/w/wholesale-rubber-cement.html?spm=a2g0o.home.search.0'];
+
+let index = ['https://www.aliexpress.us/item/3256806995885203.html','https://www.aliexpress.us/item/3256807725554752.html', 'https://www.aliexpress.us/item/3256808099472195.html','https://www.aliexpress.us/item/3256806910823785.html','https://www.aliexpress.us/item/3256806843123823.html', 'https://www.aliexpress.us/item/3256805962101282.html'];
