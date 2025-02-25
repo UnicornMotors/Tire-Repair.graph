e949ae4e6d607a94277be53f1fed2bb7cf0bf6bb
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-25 03:57:37 -0600

    Update readme.rs

--- Guide./readme.rs
+++ Guide./readme.rs
@@ -1,1 +1,3 @@
 let search.video = ['https://www.youtube.com/results?search_query=rubber+cement+tire+repair',];
+
+let guide.video = [('Rubber Cement vs. Rubber Cement','https://youtu.be/DNe2tksvJHE'];
